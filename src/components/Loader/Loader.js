6ed7a7329f02874905188f01d6f89fe63e878b0d
huxiaowen <huxiaowen@yulintu.com>
2020-01-16 18:01:16 +0800

    搭建Loader组件

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,7 @@
+const Loader = () => {
+  return (
+    <div>loader</div>
+  )
+}
+
+export default Loader
--- src/layouts/BaseLayout.js
+++ src/layouts/BaseLayout.js
@@ -1,17 +1,26 @@
-import React, {PureComponent, Fragment} from "react"
+import React, { PureComponent, Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import config from '@/utils/config';
+import { Loader } from '@/components';
 
 class BaseLayout extends PureComponent {
   render() {
-    return <Fragment>11322</Fragment>
+    return (
+      <Fragment>
+        <Helmet>
+          <title>{config.siteName}</title>
+        </Helmet>
+        <Loader />
+        111
+      </Fragment>
+    );
   }
 }
 
-export default BaseLayout
-
-
-
+export default BaseLayout;
 
 /**
  * PureComponent：以浅层对比props和state的方法实现了shouldComponentUpdate()函数；
- * Fragment：在不额外创建DOM元素的情况下，让render()方法中返回多个元素
+ * Fragment：在不额外创建DOM元素的情况下，让render()方法中返回多个元素;
+ * Helmet：动态修改网页的title；
  */
